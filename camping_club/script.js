3d14d854afd751eedaac544a22bc1033e1e489d0
Alicia <130313978+tanningexpert@users.noreply.github.com>
2023-08-09 15:18:03 -0500

    added first drop down menu; added slide animations to search and menu buttons

---
+++ camping_club/script.js
@@ -0,0 +1,26 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+
+    //DISPLAY KEYWORD SEARCH AND BUTTONS WHEN SEARCH CLICKED
+const searchInputBarWithBtns = document.getElementById("search-btn-wrapper");
+console.log(searchInputBarWithBtns);
+const searchBtn = document.getElementById("search-btn");
+console.log(searchBtn);
+const test = document.getElementsByClassName("search-btn-wrapper");
+console.log(test);
+
+searchBtn.addEventListener("click", () => {
+    searchInputBarWithBtns.style.width = "100%";
+    searchInputBarWithBtns.style.transition = "width 5s"
+//USE SEARCH BUTTON TO OPEN AND CLOSE KEYWORD SEARCH
+    //BUTTON CLICKED
+    //IS KEYWORD SEARCH OPEN? IF KEYWORD SEARCH DISPLAY = "HIDDEN" 
+    //IF OPEN CLOSE IT
+    //IF CLOSED OPEN IT
+
+
+ });
+
+
+
+
+});
